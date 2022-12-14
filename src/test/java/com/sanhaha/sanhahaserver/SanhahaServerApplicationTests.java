6ab80aca81809c9154e00bd1e34cc1b319c62241
JohnPrk <uo3641493@gmail.com>
2022-12-14 16:17:17 +0900

    chore : gradle 세팅

---
+++ src/test/java/com/sanhaha/sanhahaserver/SanhahaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanhaha.sanhahaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanhahaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
