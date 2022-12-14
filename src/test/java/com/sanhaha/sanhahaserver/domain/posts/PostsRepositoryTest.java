9be3295b4038529792325feb342ca84c295cd916
JohnPrk <uo3641493@gmail.com>
2022-12-14 21:07:06 +0900

    test : feat(PostsRepository) : 게시판 글 작성 저장 테스트 코드 작성

---
+++ src/test/java/com/sanhaha/sanhahaserver/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.sanhaha.sanhahaserver.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void 초기화() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글_저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        String author = "uo5234@naver.com";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+        Assertions.assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+}
