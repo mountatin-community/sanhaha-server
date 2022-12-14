2e54e17c22da18a8aa11b5f220ec059d92d3c8f2
JohnPrk <uo3641493@gmail.com>
2022-12-14 21:03:28 +0900

    feat : 게시판 글 작성 컨트롤러, 서비스, DTO 생성

---
+++ src/main/java/com/sanhaha/sanhahaserver/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.sanhaha.sanhahaserver.service;
+
+import com.sanhaha.sanhahaserver.domain.posts.PostsRepository;
+import com.sanhaha.sanhahaserver.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
