b24477d3e6bc471720f684637db3c2e8c80d1c51
JohnPrk <uo3641493@gmail.com>
2022-12-15 16:11:57 +0900

    test : 게시판 글 수정 컨트롤러 테스트 코드 작성

--- src/test/java/com/sanhaha/sanhahaserver/web/PostsApiControllerTest.java
+++ src/test/java/com/sanhaha/sanhahaserver/web/PostsApiControllerTest.java
@@ -2,6 +2,7 @@ package com.sanhaha.sanhahaserver.web;
 
 import com.sanhaha.sanhahaserver.domain.posts.Posts;
 import com.sanhaha.sanhahaserver.domain.posts.PostsRepository;
+import com.sanhaha.sanhahaserver.web.dto.PostUpdateRequestDto;
 import com.sanhaha.sanhahaserver.web.dto.PostsSaveRequestDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -36,7 +39,7 @@ class PostsApiControllerTest {
     }
 
     @Test
-    void 등록_테스트() {
+    void 등록_WRITE_테스트() {
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
@@ -61,5 +64,44 @@ class PostsApiControllerTest {
         Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
         Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
         Assertions.assertThat(all.get(0).getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    void 수정_UPDATE_테스트() {
+        //given
+        String prevTitle = "테스트 게시글";
+        String prevContent = "테스트 게시글";
+        String prevAuthor = "테스트 작성자";
+
+        Posts savePosts = postsRepository.save(Posts.builder()
+                .title(prevTitle)
+                .content(prevContent)
+                .author(prevAuthor)
+                .build());
+
+        Long updateId = savePosts.getId();
+        String nextTitle = "테스트 수정 게시글";
+        String nextContent = "테스트 수정 콘텐츠";
+
+        PostUpdateRequestDto requestDto = PostUpdateRequestDto.builder()
+                .title(nextTitle)
+                .content(nextContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(nextTitle);
+        Assertions.assertThat(all.get(0).getContent()).isEqualTo(nextContent);
+        Assertions.assertThat(all.get(0).getAuthor()).isEqualTo(prevAuthor);
     }
 }
