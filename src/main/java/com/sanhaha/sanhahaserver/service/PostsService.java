7f049b34d935669bfc685cf7a10224f54a4b6600
JohnPrk <uo3641493@gmail.com>
2022-12-15 16:08:26 +0900

    feat : 게시판 글 수정 및 확인 컨트롤러, 서비스, DTO 생성(도메인 모델인 Posts 클래스의 수정으로 인한 영속성 컨텍스트가 자동으로 쿼리를 날림)

--- src/main/java/com/sanhaha/sanhahaserver/service/PostsService.java
+++ src/main/java/com/sanhaha/sanhahaserver/service/PostsService.java
@@ -1,6 +1,8 @@
 package com.sanhaha.sanhahaserver.service;
 
+import com.sanhaha.sanhahaserver.domain.posts.Posts;
 import com.sanhaha.sanhahaserver.domain.posts.PostsRepository;
+import com.sanhaha.sanhahaserver.web.dto.PostsResponseDto;
 import com.sanhaha.sanhahaserver.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsSaveRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
