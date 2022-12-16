d37971ac34df0e2b30a176d63354113a7677ff09
JohnPrk <uo3641493@gmail.com>
2022-12-16 21:23:19 +0900

    feat : 메인 페이지 View에 전체 게시물을 정렬해서 출력 기능 추가

--- src/main/java/com/sanhaha/sanhahaserver/web/dto/PostsListResponseDto.java
+++ src/main/java/com/sanhaha/sanhahaserver/web/dto/PostsListResponseDto.java
@@ -1,22 +1,22 @@
 package com.sanhaha.sanhahaserver.web.dto;
 
 import com.sanhaha.sanhahaserver.domain.posts.Posts;
-import lombok.Getter;
 
-@Getter
-public class PostsResponseDto {
+import java.time.LocalDateTime;
+
+public class PostsListResponseDto {
 
     private Long id;
     private String title;
     private String content;
     private String author;
+    private LocalDateTime modifiedDate;
 
-    public PostsResponseDto(Posts entity) {
+    public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getContent();
+        this.modifiedDate = entity.getModifiedDate();
     }
-
-
 }
