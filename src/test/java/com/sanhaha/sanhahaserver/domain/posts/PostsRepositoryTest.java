9c9513adb490c4ab3e5a8ee0fc21170eff84de1e
JohnPrk <uo3641493@gmail.com>
2022-12-16 13:45:33 +0900

    test : Posts 엔티티 생성 시점 및 수정 시점 테스트 작성(실패 - issue 작성)

--- src/test/java/com/sanhaha/sanhahaserver/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sanhaha/sanhahaserver/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.sanhaha.sanhahaserver.domain.posts;
 
+import com.sanhaha.sanhahaserver.service.PostsService;
+import com.sanhaha.sanhahaserver.web.dto.PostsSaveRequestDto;
+import com.sanhaha.sanhahaserver.web.dto.PostsUpdateRequestDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -44,5 +47,44 @@ class PostsRepositoryTest {
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
         Assertions.assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    void BaseTimeEntity가_extends_된_Posts_클래스_확인() {
+        //given
+        String prevTitle = "테스트 게시글";
+        String prevContent = "테스트 본문";
+        String prevAuthor = "uo5234@naver.com";
+
+        Posts savePosts = postsRepository.save(Posts.builder()
+                .title(prevTitle)
+                .content(prevContent)
+                .author(prevAuthor)
+                .build());
+
+        Long savePostsId = savePosts.getId();
+        LocalDateTime now = LocalDateTime.now();
+
+        String nextTitle = "테스트 수정 게시글";
+        String nextContent = "테스트 수정 본문";
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(nextTitle)
+                .content(nextContent)
+                .build();
+        PostsService postsService = new PostsService(postsRepository);
+        postsService.update(savePostsId, requestDto);
+
+        LocalDateTime now2 = LocalDateTime.now();
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(posts.getCreatedDate());
+        System.out.println(now);
+        System.out.println(posts.getModifiedDate());
+        System.out.println(now2);
     }
 }
