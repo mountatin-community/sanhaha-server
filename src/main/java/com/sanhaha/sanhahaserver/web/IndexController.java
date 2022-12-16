dd1815a1451ba908abf6f92476db726c0c765bb5
JohnPrk <uo3641493@gmail.com>
2022-12-16 17:07:23 +0900

    feat : 게시글 등록 View 생성

--- src/main/java/com/sanhaha/sanhahaserver/web/IndexController.java
+++ src/main/java/com/sanhaha/sanhahaserver/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.sanhaha.sanhahaserver.web;
 
+import com.sanhaha.sanhahaserver.domain.posts.Posts;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("posts/save")
+    public String save(Model model) {
+        model.addAttribute("posts", new Posts());
+        return "posts-save";
     }
 }
