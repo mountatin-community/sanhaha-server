eb80fb2d785c46627cb1488d6dcb84da88979116
JohnPrk <uo3641493@gmail.com>
2022-12-19 18:33:52 +0900

    feat : 게시물 수정 View 생성

--- src/main/java/com/sanhaha/sanhahaserver/web/IndexController.java
+++ src/main/java/com/sanhaha/sanhahaserver/web/IndexController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +22,11 @@ public class IndexController {
     @GetMapping("posts/save")
     public String save() {
         return "posts-save";
+    }
+
+    @GetMapping("posts/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        model.addAttribute("posts", postsService.findById(id));
+        return "posts-update";
     }
 }
